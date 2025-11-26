961d6f1ffb1255031f3faebff5de91a7fe1ac861
torrestor <torres.jovem@gmail.com>
2025-11-26 18:39:38 -0300

    Router + this.route.navigate + CadastroService + [BehaviorSubject + asObservable() + localStorage] => manter os dados para passagem de tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { CommonModule } from '@angular/common';
 
 const COMPONENTS = [
   HeaderComponent,
