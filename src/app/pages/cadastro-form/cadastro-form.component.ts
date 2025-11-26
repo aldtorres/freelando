d423cf930e9716d5d3ed08d1021a48f62bb19d36
torrestor <torres.jovem@gmail.com>
2025-11-26 15:23:07 -0300

    FormGroup - FormBuilder - Validators.required - this.cadastroForm.valid - <form [formGroup]="cadastroForm" - [disabled]="cadastroForm.invalid"

--- src/app/pages/cadastro-form/cadastro-form.component.ts
+++ src/app/pages/cadastro-form/cadastro-form.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RadioOptionComponent } from "../../shared/components/radio-option/radio-option.component";
+import { ExperienceLevelComponent } from "../../shared/components/experience-level/experience-level.component";
+import { ButtonComponent } from "../../shared/components/button/button.component";
 
 const MODULES = [
   CommonModule,
@@ -11,12 +14,18 @@ const MODULES = [
   selector: 'app-cadastro-form',
   standalone: true,
   imports: [
-    ...MODULES
-  ],
+    ...MODULES,
+    RadioOptionComponent,
+    ExperienceLevelComponent,
+    ButtonComponent
+],
   templateUrl: './cadastro-form.component.html',
   styleUrls: ['./cadastro-form.component.scss']
 })
-export class CadastroFormComponent {
+export class CadastroFormComponent implements OnInit {
+
+  //atenção este é o pivo tanto no *.HTML  quanto no *.TS
+  //também usando *.HTML => [formGroup]="cadastroForm" e  [disabled]="cadastroForm.invalid"
   cadastroForm!: FormGroup;
 
   areasAtuacao = [
@@ -45,4 +54,42 @@ export class CadastroFormComponent {
       description: '(6 anos ou mais)'
     }
   ];
+
+
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+
+    //procurar [areasAtuacao e niveisExperiencia] em =>> src/app/pages/cadastro-form/cadastro-form.component.html
+    this.cadastroForm = this.fb.group({
+      areasAtuacao: ['', Validators.required ], //ref.00.atu - inicial com string vazia
+      niveisExperiencia: ['', Validators.required] //ref.01.niv
+    });
+
+  }
+
+  onAreaChange(area: string) {
+    this.cadastroForm.get('areasAtuacao')?.setValue(area);//ref.00.atu
+  }
+
+  onNivelChange(nivel: string) {
+    this.cadastroForm.get('niveisExperiencia')?.setValue(nivel);//ref.01.niv    
+  }
+
+  onAnterior() {
+    console.log('Voltar')
+  }
+
+  onProximo() {
+    if(this.cadastroForm.valid){
+      console.log('OK')
+    }else{
+      console.log('nOK')
+    }
+  }
+
+
+
 }
