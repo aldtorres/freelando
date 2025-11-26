872dc8cc21027d7c4dbf09c380cf947b968fe77d
torrestor <torres.jovem@gmail.com>
2025-11-26 15:29:08 -0300

    ROTAS - adicionando componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
-import { CadastroFormComponent } from './pages/cadastro-form/cadastro-form.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { CommonModule } from '@angular/common';
@@ -10,7 +9,6 @@ import { CommonModule } from '@angular/common';
 const COMPONENTS = [
   HeaderComponent,
   FooterComponent,
-  CadastroFormComponent
 ];
 
 const MODULES = [
@@ -21,8 +19,9 @@ const MODULES = [
   selector: 'app-root',
   imports: [
     ...COMPONENTS,
-    ...MODULES
-  ],
+    ...MODULES,
+    RouterOutlet
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
