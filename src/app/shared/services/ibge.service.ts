31c1c10c3681ffe6b32d5d1cf2f6837088f84102
torrestor <torres.jovem@gmail.com>
2025-11-27 15:12:24 -0300

    ibge.service - implementação

--- src/app/shared/services/ibge.service.ts
+++ src/app/shared/services/ibge.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, retry } from 'rxjs';
+
+export interface IEstado{
+  id:number;
+  sigla:string;
+  nome:string;
+}
+
+export interface ICidade{
+  id:number;
+  nome:string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class IbgeService {
 
-  constructor() { }
+  private readonly API_URL = 'https://servicodados.ibge.gov.br/api/v1/localidades';
+
+  constructor(private http: HttpClient) { }
+
+  //obs.: httpClient  já retorna em sua estrutura um "Observable"
+  //Observable usar metodos assinc
+  getEstados(): Observable<IEstado[]>{
+    //.pipe(retry(2)) =>  caso ocorra falha tenta 2 vezes
+    return this.http.get<IEstado[]>(`${this.API_URL}/estados?orderBy=nome`)
+      .pipe(
+        retry(2)
+      );
+  }
+
+  getCidadesPorEstado(uf: string): Observable<ICidade[]>{
+    //.pipe(retry(2)) =>  caso ocorra falha tenta 2 vezes
+    return this.http.get<ICidade[]>(`${this.API_URL}/estados/${uf}/municipios?orderBy=nome`)
+      .pipe(
+        retry(2)
+      );
+  }
 }
